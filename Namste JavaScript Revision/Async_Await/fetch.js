cff04b4a6b7c5ca40e5b6f829fbbe7cba1246d66
Sarvesh207 <sarveshgaynar@gmail.com>
2023-10-29 20:45:08 +0530

    practice romises and async and await

--- Namste JavaScript Revision/Async_Await/fetch.js
+++ Namste JavaScript Revision/Async_Await/fetch.js
@@ -1,11 +1,22 @@
-let url = "https://api.github.com/users/Sarvesh207"
+let url = "https://api.github.com/user/Sarvesh207";
 
-// 
-function getData(){
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err))
+//  handle api using .then/.catch
+// function getData() {
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+// }
+
+// getData();
+
+async function getApiData(){
+  try {
+      const data  =  await fetch(url);
+      const result = await data.json();
+      console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-getData();
+getApiData();
